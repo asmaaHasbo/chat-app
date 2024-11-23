0d9714f615a1a8acbb0aa71ff706b6885b8812fb
asmaa <elsaeeedasmaa@gmail.com>
2024-11-23 12:41:44 +0200

    create files for splash

--- lib/core/themes/styles.dart
+++ lib/core/themes/styles.dart
@@ -1,17 +1,17 @@
-import 'package:flutter/material.dart';
-
-import 'colors.dart';
-
-abstract class Styles {
-  static TextStyle textStyle45 = TextStyle(
-    fontSize: 30,
-    color: AppColors.mainColor,
-    fontWeight: FontWeight.w900,
-  );
-
-  static TextStyle textStyle22 = const TextStyle(
-    fontSize: 22,
-    color: Colors.black,
-    fontWeight: FontWeight.w500,
-  );
-}
+// import 'package:flutter/material.dart';
+//
+// import 'colors.dart';
+//
+// abstract class Styles {
+//   static TextStyle textStyle45 = TextStyle(
+//     fontSize: 30,
+//     color: AppColors.mainColor,
+//     fontWeight: FontWeight.w900,
+//   );
+//
+//   static TextStyle textStyle22 = const TextStyle(
+//     fontSize: 22,
+//     color: Colors.black,
+//     fontWeight: FontWeight.w500,
+//   );
+// }
