0480d23ff19328daf858a019bcb3fdc2dadbe0b9
asmaa <elsaeeedasmaa@gmail.com>
2024-11-23 12:49:48 +0200

    created splash screen done ✔️🤘

--- lib/core/shared_widgets/logo.dart
+++ lib/core/shared_widgets/logo.dart
@@ -1,12 +1,12 @@
-// import 'package:flutter/cupertino.dart';
-//
-// import '../../../../core/constants/assests_images.dart';
-//
-// class Logo extends StatelessWidget {
-//   const Logo({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return  Image.asset(logo , width: 200,);
-//   }
-// }
+import 'package:flutter/cupertino.dart';
+
+import '../../../../core/constants/assests_images.dart';
+
+class Logo extends StatelessWidget {
+  const Logo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Image.asset(logo , width: 200,);
+  }
+}
